51dfaa7b434f921e708b36092d3825721017881d
niechengyang <niechengyang@bytedance.com>
2022-12-04 19:21:10 +0800

    feat: 初始化项目配置

---
+++ packages/test.ts
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2021 ByteDance Inc. All Rights Reserved
+ *
+ * This source code is licensed under the Apache license.
+ * See LICENSE file in the project root for license information.
+ *
+ * @file: [功能描述]
+ *
+ * @date: 2022/12/4 19:20:52
+ * @author: 聂成阳(niechengyang@bytedance.com)
+ */
+console.log('init')
